fa08d014ad2eb4c50e943e50ae095a5db6f837fa
syd <2965437507@qq.com>
2020-04-03 19:12:12 +0800

    initial update2

--- lianxi.py
+++ lianxi.py
@@ -6,3 +6,12 @@
 teacher={'admin1':'123456','admin2':'987654321','mm':'123'}
 
 student={'BZB':100,'bzb':80,'Bzb':95}
+
+from tkinter import*
+from tkinter import messagebox
+
+top=Tk()
+top.title('学生管理系统登录页面')
+
+label1=Label(top,text='用户名:')
+label2=Label(top,text='密码:')
